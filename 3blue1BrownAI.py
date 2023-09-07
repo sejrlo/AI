1ec4931ed1e2588fed5a3888605a345c8811fde8
Sejrlo <sejrlo@gmail.com>
2023-09-07 08:50:50 +0200

    made the backprop function but it doesnt work

--- 3blue1BrownAI.py
+++ 3blue1BrownAI.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class neural_network():
-    def __init__(self, inputs, outputs, hiddenlayers):
+    def __init__(self, inputs, outputs, hiddenlayers, learning_factor=1):
+        self.learning_factor = learning_factor
         self.inputs=inputs
         self.outputs = outputs 
-        self.layers = len(hiddenlayers) + 1
+        self.layers = [inputs] + hiddenlayers + [len(outputs)]
         self.weights = [np.random.rand(inputs, hiddenlayers[0])]
         for i in range(1,len(hiddenlayers)):
             self.weights.append(np.random.rand(hiddenlayers[i-1], hiddenlayers[i]))
@@ -12,7 +13,7 @@ class neural_network():
         self.weights.append(np.random.rand(hiddenlayers[-1], len(outputs)))
 
 
-        self.biasis = [np.random.rand(inputs)]
+        self.biasis = []
         for layer in hiddenlayers:
             self.biasis.append(np.random.rand(layer))
         
@@ -24,12 +25,12 @@ class neural_network():
             if len(data) == self.inputs:
                 input = np.array(data)
                 layers = [input]
-                for i in range(self.layers):
+                for i in range(len(self.layers)-1):
                     result = np.dot(layers[i], self.weights[i]) + self.biasis[i]
                     layer = self.sigmoid(result)
                     # Calculate the sum of each row
                     layers.append(layer)
-                if all_neurons: return layers[-1]
+                if not all_neurons: return layers[-1]
                 else: return layers
             else:
                 raise Exception(f"Inputs must be equal to the number of input neurons: {self.inputs}")
@@ -43,6 +44,10 @@ class neural_network():
             print("desired output", desired_outputs[i])
 
     def backprop(self, layers, desired_ouputs):
+        change_to_neurons = []
+        for layer in self.layers:
+            change_to_neurons.append(np.array([0] * layer))
+
         changes_to_weights = []
         for weights in self.weights:
             changes_to_weights.append(weights*0)
@@ -51,13 +56,16 @@ class neural_network():
         for bias in self.biasis:
             changes_to_biasis.append(bias*0)
 
-        for i in range(len(changes_to_weights)-1,-1):
-            changes_to_weights[i] = layers[i]
+        for i in range(len(layers)-1,0):
+            if i == len(layers)-1:
+                change_to_neurons[i] = 2*(layers[i]-desired_ouputs)
+            else:
+                change_to_neurons[i] = weights[i+1] * self.dif_sigmoid(self.weights[i+1]*layers[i] + self.biasis[i+1]) * change_to_neurons[i+1]
 
-
-
-
+            changes_to_weights[i] = layers[i-1]*self.dif_sigmoid(self.weights[i]*layers[i-1] + self.biasis[i])*change_to_neurons[i]
+            changes_to_biasis[i] = self.dif_sigmoid(self.weights[i]*layers[i-1] + self.biasis[i])*change_to_neurons[i]
         
+        return changes_to_weights, changes_to_biasis
 
     def train(self, data, desired_outputs):
         if len(data) != len(desired_outputs): raise Exception("length of data must be equal to the length of desired_outputs")
@@ -71,18 +79,30 @@ class neural_network():
             changes_to_biasis.append(bias*0)
 
         for i in range(len(data)):
-            layers = self.test(data[i], True)
+            layers = self.run(data[i], True)
             weights, biasis = self.backprop(layers, desired_outputs[i])
 
             for i in range(len(weights)):
                 changes_to_weights[i] += weights[i]
+            
+            for i in range(len(biasis)):
+                changes_to_biasis[i] += biasis[i]
 
-            changes_to_biasis += biasis
+        print("change_to_weights", changes_to_weights)
+        print("change_to_biasis", changes_to_biasis)
+
+        for change in changes_to_weights:
+            change = (change * self.learning_factor)/len(data)
+
+        for change in changes_to_biasis:
+            change = (change * self.learning_factor)/len(data)
+
         
         for i in range(len(self.weights)):
             self.weights[i] += changes_to_weights[i]
-
-        self.biasis += changes_to_biasis
+        
+        for i in range(len(self.biasis)):
+            self.biasis[i] += changes_to_biasis[i]
 
         return changes_to_biasis, changes_to_weights
 
@@ -92,8 +112,19 @@ class neural_network():
     def sigmoid(self, array):
         return 1/(1+np.exp(-array))
 
-nn = neural_network(1,["1","2"],[2,2])
-nn.test([0.5])
+
+nn = neural_network(1,["0","1"],[3,3], 100000)
+
+data = []
+desired_outputs = []
+
+for i in range(100000):
+    data.append([i%2])
+    desired_outputs.append([(i+1)%2,i%2])
+
+
+nn.train(data, desired_outputs)
+nn.test([[0],[1],[0],[1]], [[1,0],[0,1],[1,0],[0,1]])
 
 
 
