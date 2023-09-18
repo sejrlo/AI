233261cc4e3564f9e132d1bb37925f39a80cec6c
unknown <sejrlo@gmail.com>
2023-09-19 00:00:07 +0200

    changes to the AI framework and done with the tic-tac-toe game

--- Kryds_Bolle_I_Kryds_Bolle.py
+++ Kryds_Bolle_I_Kryds_Bolle.py
@@ -14,28 +14,133 @@ r = 40
 mouseDown = False
 
 boardstart = (310,32)
-space_size = 220
+_space_size = 220
+
+turn = "X"
+
+last_space_index = None
 
 class Board():
-    def __init__(self, boardstart, space_size, is_clickable = True, has_children = False):
+    def __init__(self, boardstart, space_size, color, is_clickable = True, has_children = False, parent = None):
         self.boardstart = boardstart
         self.space_size = space_size
         self.is_clickable = is_clickable
         self.children = []
+        self.color = color
+        self.spaces = [None] * 9
+        self.resolved = "not"
+        self.parent = parent
+
         if has_children:
-            for x in range(self.boardstart[0], self.boardstart[0] + 3*self.space_size, space_size):
-                for y in range(self.boardstart[1], self.boardstart[1] + 3*self.space_size, space_size):
-                    self.children.append(Board((x+5, y+5), int(self.space_size/3)-10))
+            for x in range(3):
+                for y in range(3):
+                    self.children.append(Board((self.boardstart[0] + x*self.space_size + 7, self.boardstart[1] + y*self.space_size+7), int(space_size/3)-5, "red", parent=self))
     
     def draw(self):
-        for x in range(self.boardstart[0], self.boardstart[0] + 3*self.space_size, space_size):
-            for y in range(self.boardstart[1], self.boardstart[1] + 3*self.space_size, space_size):
-                pg.draw.rect(screen, "black", (x, y, space_size, space_size), width=1)
-        
+        for x in range(3):
+            for y in range(3):
+                pg.draw.rect(screen, self.color, (self.boardstart[0] + x*self.space_size, self.boardstart[1] + y*self.space_size, self.space_size, self.space_size), width=1)
+                if self.spaces[3*x+y] == "X":
+                    pg.draw.line(screen, self.color, (self.boardstart[0] + x*self.space_size + 5, self.boardstart[1] + y*self.space_size + 5), 
+                                 (self.boardstart[0] + x*self.space_size - 5 + self.space_size, self.boardstart[1] + y*self.space_size - 5 + self.space_size))
+                    pg.draw.line(screen, self.color, (self.boardstart[0] + x*self.space_size + 5, self.boardstart[1] + y*self.space_size - 5 + self.space_size), 
+                                 (self.boardstart[0] + x*self.space_size - 5 + self.space_size, self.boardstart[1] + y*self.space_size + 5))
+
+                elif self.spaces[3*x+y] == "O":
+                    pg.draw.circle(screen, self.color, 
+                                   ((self.boardstart[0] + x*self.space_size - 5 + self.space_size - (self.boardstart[0] + x*self.space_size + 5))/2
+                                    + self.boardstart[0] + x*self.space_size + 5, 
+                                    (self.boardstart[1] + y*self.space_size - 5 + self.space_size - (self.boardstart[1] + y*self.space_size + 5))/2 
+                                    + self.boardstart[1] + y*self.space_size + 5), 
+                                    (self.boardstart[0] + x*self.space_size - 5 + self.space_size - (self.boardstart[0] + x*self.space_size + 5))/2 - 2, width=1)
+
+
         for child in self.children:
             child.draw()
     
-board = Board(boardstart, space_size, has_children=True, is_clickable=False)
+    def check_if_resolved(self):
+        #check for vertical win
+        for x in range(3):
+            piece = ""
+            for y in range(3):
+                if piece == "":
+                    if self.spaces[3*x+y] != None:
+                        piece = self.spaces[3*x+y]
+                    else: break
+                elif piece != self.spaces[3*x+y]: break
+            else:
+                self.resolved = piece
+                break
+        
+        #check for horizontal win
+        for y in range(3):
+            piece = ""
+            for x in range(3):
+                if piece == "":
+                    if self.spaces[3*x+y] != None:
+                        piece = self.spaces[3*x+y]
+                    else: break
+                elif piece != self.spaces[3*x+y]:
+                    break
+            else:
+                self.resolved = piece
+                break
+        
+        #check for up-left to down-right win
+        for piece in ["X", "O"]:
+            for i in range(3):
+                if self.spaces[i*4] != piece:
+                    break
+            else:
+                self.resolved = piece
+                break
+
+        #check for down-left to up-right win
+        for piece in ["X", "O"]:
+            for i in range(3):
+                if self.spaces[i*3+(2-i)] != piece:
+                    break
+            else:
+                self.resolved = piece
+                break
+        
+        if self.resolved != "not": 
+            if self.parent != None:
+                self.parent.child_resolved(self)
+            else:
+                print(self.resolved, "won")
+                global running
+                running = False
+
+    def child_resolved(self, _child):
+        for i in range(len(self.children)):
+            if self.children[i] == _child:
+                self.spaces[i] = _child.resolved
+
+        self.check_if_resolved()
+
+    def check_if_clicked(self, mouse_pos):
+        global last_space_index
+        if self.is_clickable:
+            if self.resolved == "not":
+                for x in range(3):
+                    if mouse_pos[0] > self.boardstart[0] + x*self.space_size and mouse_pos[0] < self.boardstart[0] + x*self.space_size + self.space_size:
+                        for y in range(3):
+                            if mouse_pos[1] > self.boardstart[1] + y*self.space_size and mouse_pos[1] < self.boardstart[1] + y*self.space_size + self.space_size:
+                                global turn
+                                if self.spaces[3*x+y] is None:
+                                    self.spaces[3*x+y] = turn
+                                    self.check_if_resolved()
+                                    turn = "O" if turn == "X" else "X"
+                                    last_space_index = 3*x+y
+
+        if self.children:
+            for i, child in enumerate(self.children):
+                if i == last_space_index or last_space_index is None or self.children[last_space_index].resolved != "not":
+                    child.check_if_clicked(mouse_pos)
+        
+    
+board = Board(boardstart, _space_size, "black", has_children=True, is_clickable=False)
 
 
 while running:
@@ -54,20 +159,28 @@ while running:
         #pg.draw.rect(screen, "red", (10,10,200,200), width=1)
 
         board.draw()
+        if turn == "X":
+            #draw X
+            pg.draw.line(screen, "purple", (30, 30), (100, 100))
+            pg.draw.line(screen, "purple", (30, 100), (100, 30))
+            
+        else: 
+            #draw O
+            pg.draw.circle(screen, "purple", (65, 65), 35, width=1)
 
+        
+        
     
         
 
         
-        # if mouse.get_pressed()[0]: 
-        #     if not mouseDown:  
-        #         mouseDown = True
-        #         mouse_pos = mouse.get_pos()
-        #         if ((player_pos[0] - mouse_pos[0])**2 + (player_pos[1] - mouse_pos[1])**2)** (1/2) <= r:
-        #             print(player_pos)
-        #             print(mouse.get_pos())
-        # else: 
-        #     mouseDown = False
+        if mouse.get_pressed()[0]: 
+            if not mouseDown:  
+                mouseDown = True
+                mouse_pos = mouse.get_pos()
+                board.check_if_clicked(mouse_pos)
+        else: 
+            mouseDown = False
 
 
 
@@ -80,7 +193,6 @@ while running:
         dt = clock.tick(60) / 1000
     except KeyboardInterrupt:
         break
-
 
 
 pg.quit()
--- sentdexNeuralnetwork.py
+++ sentdexNeuralnetwork.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib
 import nnfs 
 from nnfs.datasets import spiral_data
+from random import randint
 
 class Layer_Dense():
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
  
 
@@ -24,6 +25,7 @@ class Layer_Dense():
 
 class Activation_ReLU():
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
     
     def backward(self, dvalues):
@@ -32,21 +34,18 @@ class Activation_ReLU():
 
 class Activation_Softmax():
     def forward(self, inputs):
+        self.inputs = inputs
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        self.output = exp_values/np.sum(exp_values, axis=1, keepdims=True)
-        return self.output
+        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
     def backward(self, dvalues):
         self.dinputs = np.empty_like(dvalues)
-
         for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             single_output = single_output.reshape(-1,1)
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
 
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
         
-        
-
 class Loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
@@ -58,31 +57,223 @@ class Loss_CategoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
         samples = len(y_pred)
 
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
 
+        
         elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
 
         neg_log = -np.log(correct_confidences)
         return neg_log
+    
     def backward(self, dvalues, y_true):
         samples = len(dvalues)
         labels = len(dvalues[0])
-
-        if len(y_true.shape()) == 1:
+        if len(y_true.shape) == 1:
             y_true = np.eye(labels)[y_true]
 
-        self.dinputs = -y_true/dvalues
+        #print("dv", dvalues[:5])
+        self.dinputs = -y_true / dvalues
         self.dinputs = self.dinputs / samples
+        return self.dinputs
+
+class Optimizer_Adam:
+    # Initialize optimizer - set settings,
+    def __init__(self, learning_rate=0.001, decay=0., epsilon = 1e-7, beta_1 = .9, beta_2 = .999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+    def optimize(self, layers):
+        self.pre_update_params()
+        for layer in layers:
+            self.update_params(layer)
+
+        self.post_update_params()
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1. + self.decay * self.iterations)
+
+    # Update parameters
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+            
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+        
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1-self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1-self.beta_1) * layer.dbiases
+
+        weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+
+
+        layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.dweights ** 2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.dbiases ** 2
+
+        weight_cache_corrected = layer.weight_cache / (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
+
+        layer.weights += -self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
+        layer.biases += -self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+    def __str__(self):
+        if self.current_learning_rate == self.learning_rate:
+            return f"Lr: {self.learning_rate}\nDecay: {self.decay}\nEpsilon: {self.epsilon}\nBeta_1: {self.beta_1}\nBeta_2: {self.beta_2}"
+        else:
+            return f"Lr: {self.learning_rate}\nCur_Lr: {self.current_learning_rate}\nDecay: {self.decay}\nIterations: {self.iterations}\nEpsilon: {self.epsilon}\nBeta_1: {self.beta_1}\nBeta_2: {self.beta_2}"
+      
+class Optimizer_RMSProp:
+    # Initialize optimizer - set settings,
+    def __init__(self, learning_rate=0.001, decay=0., epsilon = 1e-7, rho = .9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.rho = rho
+        self.epsilon = epsilon
+
+    def optimize(self, layers):
+        self.pre_update_params()
+        for layer in layers:
+            self.update_params(layer)
+
+        self.post_update_params()
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1. + self.decay * self.iterations)
+
+    # Update parameters
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.dweights ** 2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbiases ** 2
+
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+    def __str__(self):
+        if self.current_learning_rate == self.learning_rate:
+            return f"Lr: {self.learning_rate}\nDecay: {self.decay}\nEpsilon: {self.epsilon}\nRho: {self.rho}"
+        else:
+            return f"Lr: {self.learning_rate}\nCur_Lr: {self.current_learning_rate}\nDecay: {self.decay}\nIterations: {self.iterations}\nEpsilon: {self.epsilon}\nRho: {self.rho}"
+        
+class Optimizer_AdaGrad:
+    # Initialize optimizer - set settings,
+    def __init__(self, learning_rate=1., decay=0., epsilon = 1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    def optimize(self, layers):
+        self.pre_update_params()
+        for layer in layers:
+            self.update_params(layer)
+
+        self.post_update_params()
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1. + self.decay * self.iterations)
+
+    # Update parameters
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_cache = layer.dweights ** 2
+        layer.bias_cache = layer.dbiases ** 2
+
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+    def __str__(self):
+        if self.current_learning_rate == self.learning_rate:
+            return f"Lr: {self.learning_rate}\nDecay: {self.decay}\nEpsilon: {self.epsilon}"
+        else:
+            return f"Lr: {self.learning_rate}\nCur_Lr: {self.current_learning_rate}\nDecay: {self.decay}\nIterations: {self.iterations}\nEpsilon: {self.epsilon}"
+
+class Optimizer_SGD:
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = momentum
+
+    def optimize(self, layers):
+        self.pre_update_params()
+        for layer in layers:
+            self.update_params(layer)
+
+        self.post_update_params()
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate/(1. + self.decay * self.iterations)
+
+    # Update parameters
+    def update_params(self, layer):
+        if self.momentum:
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+    
+            weights_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.weight_momentums = weights_updates
+            layer.bias_momentums = bias_updates
+        
+        else:
+            weights_updates -= self.current_learning_rate * layer.dweights
+            bias_updates -= self.current_learning_rate * layer.dbiases
+
+        layer.weights += weights_updates
+        layer.biases += bias_updates
+
+    def post_update_params(self):
+        self.iterations += 1
+
+    def __str__(self):
+        if self.current_learning_rate == self.learning_rate:
+            return f"Lr: {self.learning_rate}\nDecay: {self.decay}\nMomentum: {self.momentum}"
+        else:
+            return f"Lr: {self.learning_rate}\nCur_Lr: {self.current_learning_rate}\nDecay: {self.decay}\nIterations: {self.iterations}\nMomentum: {self.momentum}"
 
 class Neural_Network():
-    def __init__(self, inputs, hiddenlayers, outputs, loss_function):
+    def __init__(self, inputs, hiddenlayers, outputs, loss_function, optimizer):
         self.inputs = inputs
         self.outputs = outputs
         self.loss_function = loss_function
+        self.optimizer = optimizer
         if len(hiddenlayers) == 0: self.layers = [Layer_Dense(inputs, len(outputs)), Activation_ReLU()] 
         else:
             self.layers = [Layer_Dense(inputs, hiddenlayers[0]), Activation_ReLU()]
@@ -92,29 +283,89 @@ class Neural_Network():
             
             self.layers.extend([Layer_Dense(hiddenlayers[-1],len(outputs)), Activation_Softmax()])
 
-    def test(self, input, targets):
+    def run(self, input):
         self.layers[0].forward(input)
         for i in range(1,len(self.layers)):
             self.layers[i].forward(self.layers[i-1].output)
+        
+        return self.layers[-1].output
 
-        predictions = np.argmax(self.layers[-1].output, axis=1)
+    def test(self, input, targets):
+        outputs = self.run(input)
+
+        predictions = np.argmax(outputs, axis=1)
         accuracy = np.mean(predictions == targets)
 
 
-        print(self.layers[-1].output)
-        print("loss:", self.loss_function.calculate(self.layers[-1].output, targets))
+        print(outputs[:5])
+        print("loss:", self.loss_function.calculate(outputs, targets))
         print("acc:", accuracy)
-
+    
+    def train(self, input, targets, print_data=False, epoch=None):
+        outputs = self.run(input)
+        if print_data:
+            predictions = np.argmax(outputs, axis=1)
+            accuracy = np.mean(predictions == targets)
+            loss = self.loss_function.calculate(outputs, targets)
+            if not epoch is None:
+                if epoch >= 5000:
+                    print(outputs)
+                print(f"epoch: {epoch}, acc: {accuracy:.3f}, loss: {loss:.3f}, lr: {self.optimizer.current_learning_rate}")
+            else:
+                print(f"acc: {accuracy:.3f}, loss: {loss:.3f}, lr: {self.optimizer.current_learning_rate}")
+        output_gradient = self.loss_function.backward(outputs, targets)
+        for layer in reversed(self.layers):
+            output_gradient = layer.backward(output_gradient)
+            output_gradient = layer.dinputs
         
-
-
-
-
+        layers_to_optimize = []
+        for layer in self.layers:
+            if hasattr(layer, "weights"): layers_to_optimize.append(layer)
+        self.optimizer.optimize(layers_to_optimize)
+        
 nnfs.init()
 
-X, Y = spiral_data(100, 3)
+X, y = spiral_data(samples=100, classes=3)
+#print(X, y)
+y_true_data = []
+training_data = []
+batch_size = 8
+""" X = [[0,0], [1,0], [0,1], [1,1]]
+y = [0, 1, 1, 0] """
+epochs = 10001
+""" for i in range(epochs):
+    batch = []
+    y_true_batch = []
+    for u in range(batch_size):
+        d = np.random.randint(0,2,(2,))
+        y_true = int(d[0] != d[1])
+        batch.append(d)
+        y_true_batch.append(y_true)
 
-nn = Neural_Network(2, [3], ["1","2","3"], Loss_CategoricalCrossentropy())
+    training_data.append(np.array(batch))
+    y_true_data.append(np.array(y_true_batch)) """
 
-nn.test(X, Y)
+for i in range(epochs):
+    """ batch = []
+    y_true_batch = []
+    for i in range(batch_size):
+        index = randint(0, len(X)-1)
+        batch.append(X[index])
+        y_true_batch.append(y[index]) """
+    
+    training_data.append(np.array(X))
+    y_true_data.append(np.array(y))
 
+    
+
+nn = Neural_Network(2, [64], ["0", "1", "2"], Loss_CategoricalCrossentropy(), Optimizer_Adam(learning_rate=0.005, decay=5e-7))
+print(nn.optimizer)
+nn.test(np.array(X), np.array(y))
+
+for epoch, (batch, y_true_batch) in enumerate(zip(training_data, y_true_data)):
+    if not epoch % 100:
+        nn.train(X, y, print_data=True, epoch=epoch)
+    else:
+        nn.train(X, y)
+
+nn.test(np.array(X), np.array(y))
